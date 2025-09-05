8a46c49f2f221580512c7fd79949bd82327a5750
DigitalOneThe <daun.daunovich.01@bk.ru>
2025-09-06 02:20:39 +0300

    0.3dl compability samp

--- samp-keys/CHooks.h
+++ samp-keys/CHooks.h
@@ -3,17 +3,19 @@
 #define SAMP_VERSION_R1			(1)
 #define SAMP_VERSION_R3			(2)
 #define SAMP_VERSION_R5			(3)
+#define SAMP_VERSION_03DL		(4)
 
 struct SampFunctionAddrs {
-	int version;
+	uintptr_t version;
 	uintptr_t gameLoopAddr;
 	uintptr_t keyPressHandlerAddr;
 };
 
-const std::array<SampFunctionAddrs, 3> g_array_addrs = { {
+const std::array<SampFunctionAddrs, 4> g_array_addrs = { {
 	{ SAMP_VERSION_R1, 0x53BEE0, 0x5D850 },
 	{ SAMP_VERSION_R3, 0x53BEE0, 0x60BF0 }, // game loop
-	{ SAMP_VERSION_R5, 0x53BEE0, 0x61360 } // KeyPressHandler (SAMP)
+	{ SAMP_VERSION_R5, 0x53BEE0, 0x61360 }, // KeyPressHandler (SAMP)
+	{ SAMP_VERSION_03DL, 0x53BEE0, 0x60DE0  }
 } };
 
 class CHooks
--- samp-keys/Sync/SyncClient.hpp
+++ samp-keys/Sync/SyncClient.hpp
@@ -29,22 +29,25 @@ namespace Sync {
                 entrypointOffset = ntheader->OptionalHeader.AddressOfEntryPoint;
             }
         }
+
         return entrypointOffset;
     }
 
     inline uintptr_t getSampNetGame() {
         auto samp_module = getSampModule();
         if (samp_module == 0) return 0;
-        if (getSampEntryPointOffset() != 0xCC4D0) return 0;
-        return *reinterpret_cast<uintptr_t*>(samp_module + 0x26E8DC);
+        //if (getSampEntryPointOffset() != 0xCC4D0) return 0;
+        return *reinterpret_cast<uintptr_t*>(samp_module + 0x2ACA24);
     }
 
     inline RakClientInterface* getRakClientIntf() {
         auto samp_module = getSampModule();
+        if (!samp_module) return nullptr;
+
         auto samp_netgame = getSampNetGame();
-        if (samp_netgame == 0) return nullptr;
-        return reinterpret_cast<RakClientInterface * (__thiscall*)(uintptr_t)>
-            (samp_module + 0x1A40)(samp_netgame);
+        if (!samp_netgame) return nullptr;
+
+        return reinterpret_cast<RakClientInterface*>(samp_netgame + 0x2C);
     }
 
     inline void SendPressedKey(UINT key) {
